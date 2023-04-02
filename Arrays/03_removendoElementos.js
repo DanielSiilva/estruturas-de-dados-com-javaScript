ca556e565243a952a9fe56c19e8257cd05d18a5e
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-02 00:00:38 -0300

    Feat: Array => removendo elementos do inicio e fim

---
+++ Arrays/03_removendoElementos.js
@@ -0,0 +1,15 @@
+//Remover do fim do array
+
+const listNumber = [1, 2, 3, 4]
+listNumber.pop(3)
+//console.log(listNumber);
+listNumber.pop(2)
+console.log(listNumber)
+
+//Removendo do inicio do array => metodo shift
+
+listNumber.shift()
+
+console.log(listNumber);
+
+//Os metodos shift e unshift permitem que um array emule uma estrutura de dados básica de fila (queue). 
